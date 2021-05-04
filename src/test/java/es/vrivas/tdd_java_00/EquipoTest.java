b3405775e7bbe079d7d99cf9f76613b85d424ef3
Victor M. Rivas <vrivas@ujaen.es>
2021-05-04 16:20:36 +0200

    Añado mensajes de salida en los métodos setUp y tearDown

--- src/test/java/es/vrivas/tdd_java_00/EquipoTest.java
+++ src/test/java/es/vrivas/tdd_java_00/EquipoTest.java
@@ -23,22 +23,27 @@ public class EquipoTest {
     
     @BeforeClass
     public static void setUpClass() {
+        System.out.println( "setUpClass");
     }
     
     @AfterClass
     public static void tearDownClass() {
+        System.out.println( "tearDownClass");
     }
     
     @Before
     public void setUp() {
+        System.out.println( "setUp");
     }
     
     @After
     public void tearDown() {
+        System.out.println( "setUp");
     }
 
     @Test
     public void testSomeMethod() {
+        System.out.println( "testSomeMethod");
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
