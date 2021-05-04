dfbcfdac728b2fb61fa2e56f68860e83d90f4338
Victor M. Rivas <vrivas@ujaen.es>
2021-05-04 16:10:46 +0200

    Creo clase Equipo vacía

---
+++ src/main/java/es/vrivas/tdd_java_00/Equipo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.vrivas.tdd_java_00;
+
+/**
+ *
+ * @author vrivas
+ */
+public class Equipo {
+    
+}
