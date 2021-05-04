f145033ceb80759fa1e9c189086b2751fae39941
Victor M. Rivas <vrivas@ujaen.es>
2021-05-04 18:39:45 +0200

    Creo los casos de prueba para iniciaEquipos y getEquipos

--- src/main/java/es/vrivas/tdd_java_00/App.java
+++ src/main/java/es/vrivas/tdd_java_00/App.java
@@ -10,6 +10,28 @@ package es.vrivas.tdd_java_00;
  * @author vrivas
  */
 public class App {
+    // Constante para definir el máximo número de equipos
+    public static final int MAX_EQUIPOS = 20;
+    
+    // Vector con los equipos
+    static Equipo[] equipos=new Equipo[MAX_EQUIPOS];
+    
+    // Inicializa los nombres y puntos de los equipos
+    public static void iniciaEquipos() {
+        for( int i=0; i<MAX_EQUIPOS; ++i ) {
+            equipos[i]=new Equipo( "Equipo "+(i+1), 0 );
+        }
+    }
+    
+    // Devuelve el vector de equipos
+    public static Equipo[] getEquipos() {
+        return equipos;
+    }
+    
+    /**
+     * Main function
+     * @return  0 si todo va bien, otro valor si hay problemas.
+     */
     public static int main() {
         System.out.println( "Inicio ejecución del programa." );
         return 0;
--- src/test/java/es/vrivas/tdd_java_00/AppTest.java
+++ src/test/java/es/vrivas/tdd_java_00/AppTest.java
@@ -5,6 +5,8 @@
  */
 package es.vrivas.tdd_java_00;
 
+import static es.vrivas.tdd_java_00.App.MAX_EQUIPOS;
+import static es.vrivas.tdd_java_00.App.equipos;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,12 +17,20 @@ import static org.junit.Assert.*;
  * @author vrivas
  */
 public class AppTest {
+    static Equipo[] equipos=new Equipo[App.MAX_EQUIPOS];
     
     public AppTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        System.out.println( "setUpClass para AppTest" );
+        // Mock data
+        for( int i=0; i<App.MAX_EQUIPOS; ++i ) {
+            equipos[i]=new Equipo( "Equipo "+(i+1), 0 );
+            System.out.println( equipos[i]);
+
+        }
     }
     
     @AfterClass
@@ -31,6 +41,12 @@ public class AppTest {
     public void main() {
         System.out.println( "Test App.main");
         assertEquals( App.main(), 0 );
+    }
+    
+    @Test
+    public void iniciaEquipos() {
+        System.out.println( "Test iniciaEquipos" );
+        assertArrayEquals(App.getEquipos(), equipos );
     }
     
 }
